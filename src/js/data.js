9e14aaadd2dd2f921592ed1565845f8e310682a5
unknown <bakunin.abolnikov@gmail.com>
2021-01-15 18:44:49 +0200

    add Expirience

---
+++ src/js/data.js
@@ -0,0 +1,28 @@
+const data = {
+    "first_name": "Anton",
+    "last_name": "Abolnikov",
+    "job_title": "Front End Software Developer",
+    "photo": "images/w1.jpg",
+    "phone": "+972-55-99-88-7-22",
+    "email": "abolnikov.anton@gmail.com",
+    "telegram": "@abolnikov",
+    "linkedin": "https://www.linkedin.com/in/anton-abolnikov/",
+    "address": "Bat Yam, Israel",
+    "skills": [
+        "JavaScript",
+        "TypeScript",
+        "Vanilla JS",
+        "HTML & CSS",
+        "Bootstrap",
+        "jQuery",
+        "Node JS & Express",
+        "Firebase",
+        "MongoDB",
+        "Angular 8+ & RxJS",
+    ],
+    "education": [
+        ["2005-2006", "MSc in Computer science. Kharkov National University of Radio Electronics"],
+        ["2001-2005", "BSc in Applied mathematics and informatics. Kharkov National University of Radio Electronics"],
+    ],
+    "profile": ""
+};
